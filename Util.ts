c8a162872ea37c17fc61de4eb2c7503ce562d2cf
Kaan İnel <kaaninel@gmail.com>
2021-02-16 13:32:07 +0300

    Util temp dir

--- Util.ts
+++ Util.ts
@@ -4,21 +4,19 @@ import { join } from "https://deno.land/std@0.82.0/path/mod.ts";
 
 
 export class TempFile {
+	static Tmp = Deno.makeTempDirSync();
 
 	private filename: string;
 
 	constructor (
-		public generator: () => string
+		public generator: () => string,
+		extension = ""
 	) {
 		const content = generator();
 		const hash = createHash("md5");
 		hash.update(content);
 		const filename = hash.toString();
-		this.filename = join(
-			Deno.env.get("TMPDIR") ||
-			Deno.env.get("TEMPDIR") ||
-			Deno.env.get("TEMP") ||
-			"/tmp/", `dockrr_${filename}`);
+		this.filename = join(TempFile.Tmp, `dockrr_${filename}${extension}`);
 		Deno.writeTextFileSync(this.filename, content);
 	}
 
